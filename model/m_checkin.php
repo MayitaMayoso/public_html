98993bdac629b0e5ad4f4efddbc3df1d332ad7b4
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 17:23:02 +0900

    Some changes made

--- model/m_checkin.php
+++ model/m_checkin.php
@@ -1,14 +1,18 @@
 <?php
 function registre($name, $mail, $hash, $address, $connexio) {
     try {
-        $consulta_registre = $connexio->prepare("INSERT INTO CUSTOMERS (NAME, MAIL, PASSWORD, ADDRESS) VALUES (:nom, :mail, :hash, :address)");
+        $consulta_registre = $connexio->prepare("INSERT INTO CUSTOMERS (NAME, MAIL, PASSWORD, ADDRESS) VALUES ('Agustin', 'molina.uasug@gmail.com', '1234', 'calle 12')");
 
         $consulta_registre->bindValue(':mail', $mail);
         $consulta_registre->bindValue(':hash', $hash);
         $consulta_registre->bindValue(':name', $name);
         $consulta_registre->bindValue(':address', $address);
 
-        $consulta_registre->execute();
+        if( $consulta_registre->execute() ){
+            echo "Te has registrado";
+        } else {
+            echo"Error";
+        }
 
     }catch(PDOException $e)
     {echo "Error:" .$e->getMessage();}
