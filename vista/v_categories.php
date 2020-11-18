354541e361f8fb3a2093daeab0467657261f94ea
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 14:09:34 +0900

    Intentando arreglar la vista de las categorias

--- vista/v_categories.php
+++ vista/v_categories.php
@@ -1,11 +1,7 @@
 <br>
 <br>
 <?php foreach ($resultat_categories as $categoria): ?>
-	<div class=categories>
-		<h3><?php echo $categoria['NAME'] ?></h3>
-		<br>
-		<a href="/index.php?&cat=<?php echo$categoria['ID']?>">
-        	<button class="Boton" id=<?php echo$categoria['ID']?>>Ver más</button>
-        </a>
-    </div>
+	<a href="#" class="item">
+			<div><h3><?php echo $categoria['NAME'] ?></h3></div>
+	</a>
 <?php endforeach; ?>
