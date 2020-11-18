d61665b329008b516a220feef28c14ddf2bce1de
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 17:07:56 +0900

    Quitando bugs 16

--- vista/v_product.php
+++ vista/v_product.php
@@ -1,6 +1,6 @@
 <div class=product>
-	<h2><?php echo $producte_resultat[0]['NAME'];?></h2><br>
-	<p class="price"><?php echo $producte_resultat[0]['PRICE'];?></p>
-	<p><?php echo $producte_resultat[0]['DESCRIPTION'];?></p><br>
+	<h2><?php echo $producte_resultat['NAME'];?></h2><br>
+	<p class="price"><?php echo $producte_resultat['PRICE'];?></p>
+	<p><?php echo $producte_resultat['DESCRIPTION'];?></p><br>
 	<button class="carrito">Añadir al carrito</button>
 </div>
