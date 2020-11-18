d6e9ca948574c71ed936c48e5ecfc623eeb574aa
1528948 <alberto.vallespin@e-campus.uab.cat>
2020-11-18 01:54:28 +0100

    ajax

--- resource_inici.php
+++ resource_inici.php
@@ -4,7 +4,8 @@
 <head>
 	<meta charset="UTF-8" />
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<script src="js/ajax_JS.js"></script>
+	<script src="javascript/ajax.js"></script>
+	<script src="javascript/jquery-3.5.1.min.js"></script>
 	<title>Tiender - </title>
 </head>
 
