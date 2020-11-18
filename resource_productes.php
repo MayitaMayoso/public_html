9d58a8f55d84b0204985a2c21aef6833be93d61b
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 16:50:30 +0900

    Quitando bugs 8

--- resource_productes.php
+++ resource_productes.php
@@ -1,5 +1,7 @@
+<nav>
+    <?php require __DIR__.'/resource_navigator.php'; ?>
+</nav>
+
 <div class="products">
-
-<?php require __DIR__."/controlador/llistar_productes.php";?>
-
+    <?php require __DIR__."/controlador/llistar_productes.php";?>
 </div>
