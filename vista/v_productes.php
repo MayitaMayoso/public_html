5aebcf35bc094d37f403878f50abf1cb51bb0104
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 16:35:51 +0900

    Quitando bugs 3

--- vista/v_productes.php
+++ vista/v_productes.php
@@ -2,7 +2,7 @@
         <?php echo $categoria[0]['NAME']?>
 </h1>
 <ul>
-        <?php foreach ($productes as $prod):?>
+        <?php foreach ($resultat_productes as $prod):?>
                 <li>
                         <p>
                                 <a class="boto_info" href="/index.php?&product=<?php echo $prod['ID']?>">
