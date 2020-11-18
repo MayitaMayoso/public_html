cb08fed20d9f8e1437280d58d299d1342fc43697
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 14:24:48 +0900

    Sigo cambiando los productos 6

--- vista/v_categories.php
+++ vista/v_categories.php
@@ -1,7 +1,7 @@
 <br>
 <br>
 <?php foreach ($resultat_categories as $categoria): ?>
-	<a href="/index.php?&cat=<?php echo$categoria['ID']?>" class="item <?php echo$categoria['ID']?>">
+	<a href="/index.php?&cat=<?php echo$categoria['ID']?>" class="item <?php echo$categoria['NAME']?>">
 			<div><h3><?php echo $categoria['NAME'] ?></h3></div>
 	</a>
 <?php endforeach; ?>
