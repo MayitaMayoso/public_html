521b98fd05ae8bb45d2b7c836965d7fc759dc8d7
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 16:51:34 +0900

    Quitando bugs 9

--- resource_productes.php
+++ resource_productes.php
@@ -1,7 +1,24 @@
-<nav>
-    <?php require __DIR__.'/resource_navigator.php'; ?>
-</nav>
+<!DOCTYPE html>
+<html>
 
-<div class="products">
-    <?php require __DIR__."/controlador/llistar_productes.php";?>
-</div>
+<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<script src="javascript/ajax.js"></script>
+	<script src="javascript/jquery-3.5.1.min.js"></script>
+	<title>Tiender</title>
+</head>
+
+<body>
+
+	<nav>
+		<?php require __DIR__.'/resource_navigator.php'; ?>
+	</nav>
+
+	<div class="products">
+        <?php require __DIR__."/controlador/llistar_productes.php";?>
+    </div>
+
+</body>
+
+</html>
