6c2cf68efcec0e335e185335f69cefa5cf3ff4e3
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 16:59:49 +0900

    Quitando bugs 13

--- controlador/c_product.php
+++ controlador/c_product.php
@@ -3,7 +3,7 @@
 	require_once __DIR__.'/../model/m_product.php';
 
 	$connexio = connectaBD();
-	$id=$_GET['PRODUCT']?(int)$_GET['PRODUCT']:1;
+	$id=$_GET['product'];
 	$resultat_producte=getProducte($id,$connexio);
 
 	require_once __DIR__.'/../view/v_product.php';
