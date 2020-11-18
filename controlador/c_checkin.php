a362c1bca41818236542b32bd7bdd13eccc3a594
1528948 <alberto.vallespin@e-campus.uab.cat>
2020-11-18 02:27:37 +0100

    arreglo checkin 7

--- controlador/c_checkin.php
+++ controlador/c_checkin.php
@@ -17,13 +17,13 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     else {
         $connexio = connectaBD();
         if(registre($name, $mail, $password, $address))
-            require __DIR__.'/../view/v_checkin.php';
+            require __DIR__.'/../vista/v_checkin.php';
         else
-            require __DIR__.'/../view/v_checkin.php';
+            require __DIR__.'/../vista/v_checkin.php';
         }
 
 }else
-{require __DIR__.'/../view/v_checkin.php';}
+{require __DIR__.'/../vista/v_checkin.php';}
 
 ?>
 
