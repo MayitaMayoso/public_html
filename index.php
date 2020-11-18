2659dbc04a9822d1b3686057006e11c245271976
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 14:02:41 +0900

    Comprobando si ya no sale el mensaje de error en el php

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
 // index.php
 
-$action = $_GET['action'];
-
-switch ($action) {
-	case 'productes':
-		include __DIR__.'/resource_productes.php';
-		break;
-	case 'product':
-		include __DIR__.'/resource_product.php';
-		break;
-	case 'checkin':
-		include __DIR__.'/checkin.php';
-		break;
-	case 'login':
-		include __DIR__.'/login.php';
-		break;
-	default:
-		include __DIR__.'/resource_inici.php';
-		break;
+if ( isset($_GET['action']) ) {
+	$action = $_GET['action'];
+	switch ($action) {
+		case 'productes':
+			include __DIR__.'/resource_productes.php';
+			break;
+		case 'product':
+			include __DIR__.'/resource_product.php';
+			break;
+		case 'checkin':
+			include __DIR__.'/checkin.php';
+			break;
+		case 'login':
+			include __DIR__.'/login.php';
+			break;
+	}
+} else {
+	include __DIR__.'/resource_inici.php';
 }
+
 ?>
