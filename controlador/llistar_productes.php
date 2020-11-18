0f245e7a6ba14228ed93d578ab64b8344efa42c0
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 16:31:38 +0900

    Quitando bugs 23

--- controlador/llistar_productes.php
+++ controlador/llistar_productes.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../model/connectaBD.php';
 require_once __DIR__ . '/../model/m_categories.php';
 require_once __DIR__ . '/../model/m_productes.php';
 $connexio = connectaBD();
-$categoria=$_GET['CATEGORY_ID']?(int)$_GET['CATEGORY_ID']:1111;
+$categoria=$_GET['ID']?(int)$_GET['ID']:1111;
 
 $resultat_categoria=getCategoria($categoria,$connexio);
 $resultat_productes =getProduct($categoria,$connexio);
