f0cc67461ee615868d1f6605a9adb5b5c56b11e9
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 17:28:03 +0900

    Some changes made

--- model/m_checkin.php
+++ model/m_checkin.php
@@ -3,10 +3,10 @@ function registre($name, $mail, $hash, $address, $connexio) {
     try {
         $consulta_registre = $connexio->prepare("INSERT INTO CUSTOMERS (NAME, MAIL, PASSWORD, ADDRESS) VALUES (:nom, :mail, :hash, :address)");
 
-        $consulta_registre->bindValue(':mail', "'" + $mail + "'");
-        $consulta_registre->bindValue(':hash', "'" + $hash + "'");
-        $consulta_registre->bindValue(':name', "'" + $name + "'");
-        $consulta_registre->bindValue(':address', "'" + $address + "'");
+        $consulta_registre->bindValue(':mail', $mail);
+        $consulta_registre->bindValue(':hash', $hash);
+        $consulta_registre->bindValue(':name', $name);
+        $consulta_registre->bindValue(':address', $address);
 
         if( $consulta_registre->execute() ){
             echo "Te has registrado";
