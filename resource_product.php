9f088d0fa9876090668b2d5512883174cc661d02
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 17:10:54 +0900

    Some changes made

--- resource_product.php
+++ resource_product.php
@@ -1,6 +1,26 @@
-<div class="product">
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<script src="javascript/ajax.js"></script>
+	<script src="javascript/jquery-3.5.1.min.js"></script>
+	<title>Tiender</title>
+</head>
+
+<body>
+
+	<nav>
+		<?php require __DIR__.'/resource_navigator.php'; ?>
+	</nav>
+
+	<div class="product">
 
 <?php require __DIR__."/controlador/c_product.php";?>
 
 </div>
 
+</body>
+
+</html>
