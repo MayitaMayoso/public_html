a8109358cbe29ccaeb7a48fad0c9b7d4169cd139
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 13:53:48 +0900

    El navegador ahora está en un archivo separado

--- resource_inici.php
+++ resource_inici.php
@@ -12,18 +12,7 @@
 <body>
 
 	<nav>
-		<a href="index.php"><img class="logo" src="resources/logo1.png" /></a>
-
-		<div class="nav-space"></div>
-
-		<ul class="nav-links">
-			<li><a href="index.html">Inicio</a></li>
-			<li><div></div></li>
-			<li><div></div></li>
-			<li><a href="http://tdiw-e13.deic-docencia.uab.cat/?action=checkin" target="_self">Check-in</a></li>
-			<li><a href="http://tdiw-e13.deic-docencia.uab.cat/?action=login" target="_self">Log-in</a></li>
-			<li><a href=""><img class="carrito" src="resources/carrito.svg" /></a></li>
-		</ul>
+		<?php require __DIR__.'/resource_navigator.php'; ?>
 	</nav>
 
 	<div class="content">
