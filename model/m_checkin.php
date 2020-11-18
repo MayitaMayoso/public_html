d2c4b568b628ca29d17db36b11ba7b0c90f6cd32
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2020-11-18 16:21:13 +0900

    Cambios en el checkin 2

--- model/m_checkin.php
+++ model/m_checkin.php
@@ -1,10 +1,10 @@
 <?php
-function registre($name, $mail, $hash, $address) {
+function registre($name, $mail, $password, $address) {
     try {
-        $consulta_registre = $connexio->prepare("INSERT INTO CUSTOMERS (NAME, MAIL, PASSWORD, ADDRESS) VALUES (:nom, :mail, :hash, :address)");
+        $consulta_registre = $connexio->prepare("INSERT INTO CUSTOMERS (NAME, MAIL, PASSWORD, ADDRESS) VALUES (:nom, :mail, :password, :address)");
 
         $consulta_registre->bindValue(':mail', $email);
-        $consulta_registre->bindValue(':hash', $hash);
+        $consulta_registre->bindValue(':password', $password);
         $consulta_registre->bindValue(':name', $name);
         $consulta_registre->bindValue(':address', $address);
 
