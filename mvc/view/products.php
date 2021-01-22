0af5437aa3af5428caba6d69b251b4db06511e76
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 08:37:01 +0900

    Some changes made

--- mvc/view/products.php
+++ mvc/view/products.php
@@ -1,6 +1,6 @@
 <ul>
         <?php foreach ($resultat_products as $prod):?><li><p>
-                <a class="link" id="product-link" href="/index.php?action=product&product=<?php echo $prod['ID']?>" target-data=<?php echo $prod['ID']?>>
+                <a class="link" id="product-link" href="#" target-data=<?php echo $prod['ID']?>>
                 <?php echo $prod['NAME']?></a>
         </p></li><?php endforeach; ?>
 </ul>
