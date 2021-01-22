2ce363936a600a6c2d208f4157555e35b0c7a5f4
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 07:26:04 +0900

    Some changes made

--- javascript/asynchronousLoad.js
+++ javascript/asynchronousLoad.js
@@ -3,10 +3,13 @@ $(document).ready(
         var trigger = $("#category-link");
         var container = $("#content");
 
+        console.log("Joderrrrrrrr");
         trigger.on("click",
             function() {
                 var cat = $this.data("target");
                 container.Load("content.php?action=products?cat=" + cat);
+
+                console.log("Joderrrrrrrr2");
 
                 return false;
             }
