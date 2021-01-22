8b7107a0cd7728e33c8c4c9af38628663f89ee25
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 08:06:22 +0900

    Some changes made

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
 <head>
 	<meta charset="UTF-8" />
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<script type="text/javascript" src="javascript/jquery-3.5.1.min.js"></script>
-	<script type="text/javascript" src="javascript/asynchronousLoad.js"></script>
+	<script type="text/javascript" src="<?php echo __DIR__?>/javascript/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo __DIR__?>/javascript/asynchronousLoad.js"></script>
 	<title>Tiender</title>
 </head>
 
 <body>
 	<!-- NAVIGATOR OF THE PAGE, IF THE USER IS LOGGED IN IT WILL CHANGE -->
-	<?php echo __DIR__?>
 	<nav>
 		<?php require __DIR__.'/mvc/controller/navigator.php'; ?>
 	</nav>
