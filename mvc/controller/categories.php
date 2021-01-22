fc34ed9d28b522d783bbf798e6a5181b9f4ca84d
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 06:31:38 +0900

    Some changes made

--- mvc/controller/categories.php
+++ mvc/controller/categories.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once __DIR__ . '/../model/connection.php';
-require_once __DIR__ . '/../model/categories.php';
+require_once __DIR__ . '../model/connection.php';
+require_once __DIR__ . '../model/categories.php';
 
 $connexio = connectaBD();
 $resultat_categories = getCategories($connexio);
 
-require __DIR__ . '/../view/categories.php';
+require __DIR__ . '../view/categories.php';
 
 ?>
