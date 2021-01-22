c02fbc16f155950ec6c89ddd023a7b9e98da5c8f
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 08:07:21 +0900

    Some changes made

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
 	<meta charset="UTF-8" />
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<script type="text/javascript" src="<?php echo __DIR__?>/javascript/jquery-3.5.1.min.js"></script>
-	<script type="text/javascript" src="<?php echo __DIR__?>/javascript/asynchronousLoad.js"></script>
+	<script type="text/javascript" src="jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="asynchronousLoad.js"></script>
 	<title>Tiender</title>
 </head>
 
