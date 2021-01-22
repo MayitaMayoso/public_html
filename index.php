26b3ca485de9c24619098beb15c64493f57a1b7d
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 07:41:49 +0900

    Some changes made

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="UTF-8" />
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<script type="text/javascript" src="javascript/jquery-3.5.1.min.js"></script>
 	<title>Tiender</title>
 </head>
 
