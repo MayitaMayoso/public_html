2609597b8155fffde7d81396e517f6c51ca3993a
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 18:22:24 +0900

    Some changes made

--- mvc/view/navigator.php
+++ mvc/view/navigator.php
@@ -4,7 +4,7 @@
 
 <ul class="nav-links">
     <li><a href="http://tdiw-e13.deic-docencia.uab.cat/">Inicio</a></li>
-    <li><a href="http://tdiw-e13.deic-docencia.uab.cat/?action=checkin" target="_self">Check-in</a></li>
-    <li><a href="http://tdiw-e13.deic-docencia.uab.cat/?action=login" target="_self">Log-in</a></li>
-    <li><a href=""><img class="carrito" src="resources/carrito.svg" /></a></li>
+    <li><a href="http://tdiw-e13.deic-docencia.uab.cat/index.php?action=checkin" target="_self">Check-in</a></li>
+    <li><a href="http://tdiw-e13.deic-docencia.uab.cat/index.php?action=login" target="_self">Log-in</a></li>
+    <li><a href=""><img class="carrito" src="resources/index.php?carrito.svg" /></a></li>
 </ul>
