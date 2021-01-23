1ceb470a1569c9395fb36e1ed4866f876fb8edb5
1528948 <alberto.vallespin@e-campus.uab.cat>
2021-01-23 20:58:46 +0100

    login

--- mvc/model/users.php
+++ mvc/model/users.php
@@ -3,16 +3,16 @@ function login(string $mail,string $password)
 {
     try {
         $connexio=connect();
-        $bd = 'SELECT ID, `MAIL` FROM CUSTOMER where MAIL=:mail AND PASSWORD=:password LIMIT:1';
+        $bd = 'SELECT ID, `MAIL` FROM CUSTOMER where MAIL=:MAIL AND PASSWORD=:PASSWORD LIMIT:1';
         $conec = $connexio->prepare($bd);
         $conec->execute(
             [
-                'mail' => $mail,
-                'password' => $password,
+                'MAIL' => $mail,
+                'PASSWORD' => $password,
             ]
         );
         $result = $conec->fetch(PDO::FETCH_ASSOC);
-        return password_verify($password, $result['password']) ? result : null;
+        return password_verify($password, $result['PASSWORD']) ? result : null;
 
     }catch(PDOException $e){echo 'Error :' .$e->getMessage();}
 
