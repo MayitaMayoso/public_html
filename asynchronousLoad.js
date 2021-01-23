269bc103022a646d9fd1e103bb5428c43e0ee482
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 17:46:11 +0900

    Some changes made

--- asynchronousLoad.js
+++ asynchronousLoad.js
@@ -5,7 +5,7 @@ $(document).ready(
         $("#categories-link a").on("click",
             function() {
                 var cat = $(this).data("target");
-                container.load("content.php?action=product&product=" + cat);
+                container.load("content.php?action=products&cat=" + cat);
 
                 return false;
             }
@@ -13,6 +13,7 @@ $(document).ready(
         
         $("#products-link a").on("click",
             function() {
+                console.log("hola");
                 var prod = $(this).data("target");
                 container.load("content.php?action=product&product=" + prod);
 
--- mvc/view/products.php
+++ mvc/view/products.php
@@ -1,7 +1,12 @@
 <div id="products-link">
 <?php foreach ($resultat_products as $prod):?>
-        <a href="#" class="item" target-data=<?php echo $prod['ID']?>>
-                <div><h3><?php echo $prod['NAME']?></h3></div>
-        </a>
+	<a href="#" class="item" target-data=<?php echo $prod['ID']?>>
+		<div>
+			<h3>
+				<?php echo $prod['NAME']?>
+			</h3>
+		</div>
+
+	</a>
 <?php endforeach; ?>
 </div>
