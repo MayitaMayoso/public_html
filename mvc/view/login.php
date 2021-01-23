c354d296cc3fa04254195918ad38b3f43500d4e6
1528948 <alberto.vallespin@e-campus.uab.cat>
2021-01-23 20:59:56 +0100

    login

--- mvc/view/login.php
+++ mvc/view/login.php
@@ -1,8 +1,8 @@
 
 <section>
     <form method="post" action="/index.php?action=login" >
-        <input name="mail" type="mail" placeholder="Mail">
-        <input name="password" type="password" placeholder="Contraseña">
+        <input name="MAIL" type="mail" placeholder="Mail">
+        <input name="PASSWORD" type="password" placeholder="Contraseña">
         <input type="submit" value="Login">
     </form>
 </section>
