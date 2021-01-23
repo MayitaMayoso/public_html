72736a5981fb9f2289de31c6c9eefcae79f1831a
1528948 <alberto.vallespin@e-campus.uab.cat>
2021-01-23 20:21:29 +0100

    login

--- mvc/model/checkin.php
+++ mvc/model/checkin.php
@@ -1,7 +1,7 @@
 <?php
 function registre($name, $mail, $hash, $address, $connexio) {
     try {
-        $consulta_registre = $connexio->prepare("INSERT INTO `CUSTOMERS` (`NAME`, `MAIL`, `PASSWORD`, `ADDRESS) VALUES (:name, :mail, :hash, :address)");
+        $consulta_registre = $connexio->prepare("INSERT INTO `CUSTOMERS` (`NAME`, `MAIL`, `PASSWORD`, `ADDRESS`) VALUES (:name, :mail, :hash, :address)");
 
         $consulta_registre->bindValue(':mail', $mail);
         $consulta_registre->bindValue(':hash', $hash);
@@ -11,7 +11,7 @@ function registre($name, $mail, $hash, $address, $connexio) {
         if($consulta_registre->execute()){
             echo "Te has registrado";
         } else {
-            echo"Error 2";
+            echo "Error 2";
         }
 
     }catch(PDOException $e)
