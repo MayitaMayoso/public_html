82ac68fefef7e55fe43e2e3773fd45523ab1cc2e
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 18:26:37 +0900

    Some changes made

--- mvc/view/navigator.php
+++ mvc/view/navigator.php
@@ -1,10 +1,30 @@
+<script type="text/javascript">
+$(document).ready(
+        function() {
+                $("#checkin-link").on("click",
+					function() {
+						$("#content").load("content.php?action=checkin");
+						return false;
+					}
+                );
+                
+                $("#login-link").on("click",
+					function() {
+						$("#content").load("content.php?action=checkin");
+						return false;
+					}
+        		);
+        }    
+);
+</script>
+
 <a href="index.php"><img class="logo" src="resources/logo1.png" /></a>
 
 <div class="nav-space"></div>
 
 <ul class="nav-links">
     <li><a href="http://tdiw-e13.deic-docencia.uab.cat/">Inicio</a></li>
-    <li><a href="http://tdiw-e13.deic-docencia.uab.cat/index.php?action=checkin" target="_self">Check-in</a></li>
-    <li><a href="http://tdiw-e13.deic-docencia.uab.cat/index.php?action=login" target="_self">Log-in</a></li>
+    <li><a id="checkin-link" href="#" target="_self">Check-in</a></li>
+    <li><a id="login-link" href="#" target="_self">Log-in</a></li>
     <li><a href=""><img class="carrito" src="resources/carrito.svg" /></a></li>
 </ul>
