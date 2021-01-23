9fa59565b0eb06901a41fbc9cef39523a74c376b
1528948 <alberto.vallespin@e-campus.uab.cat>
2021-01-23 19:59:19 +0100

    login

--- resource_reg.php
+++ resource_reg.php
@@ -1,6 +1,6 @@
 <div class="content" id="content">		
 	<div class="registre">
-    <?php include_once __DIR__.'/controller/checkin.php';?>
+    <?php include_once __DIR__.'/mvc/controller/checkin.php';?>
 	</div>
 </div>
 
