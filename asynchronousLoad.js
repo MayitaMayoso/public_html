77663267449990df18bb60a85ba3fadc6534c3be
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-23 17:40:04 +0900

    Some changes made

--- asynchronousLoad.js
+++ asynchronousLoad.js
@@ -1,21 +1,20 @@
 $(document).ready(
     function() {
         var container = $("#content");
-        
-        $("#products-link a").on("click",
-            function() {
-                console.log("hola");
-                var prod = $(this).data("target");
-                container.load("content.php?action=product&product=" + prod);
-
-                return false;
-            }
-        );
 
         $("#categories-link a").on("click",
             function() {
                 var cat = $(this).data("target");
-                container.load("content.php?action=products&cat=" + cat);
+                container.load("content.php?action=product&product=" + cat);
+
+                return false;
+            }
+        );
+        
+        $("#products-link a").on("click",
+            function() {
+                var prod = $(this).data("target");
+                container.load("content.php?action=product&product=" + prod);
 
                 return false;
             }
