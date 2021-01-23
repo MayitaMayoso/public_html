45b2d8300653493a9e3ae7a6366d71788736b4b9
1528948 <alberto.vallespin@e-campus.uab.cat>
2021-01-24 00:23:45 +0100

    login

---
+++ mvc/model/users.php
@@ -0,0 +1,21 @@
+<?php
+function login(string $mail,string $password)
+{
+    try {
+        $connexio=connect();
+        $bd = 'SELECT * FROM CUSTOMER where MAIL=:MAIL LIMIT 1';
+        $conec = $connexio->prepare($bd);
+        $conec->execute(
+            [
+                'MAIL' => $mail,
+                #'PASSWORD' => $password,
+            ]
+        );
+        $result = $conec->fetch(PDO::FETCH_ASSOC);
+        if ($result == false)
+            return null;
+        return password_verify($password,$result['PASSWORD']) ? $result : 0;
+
+    }catch(PDOException $e){echo 'Error :' .$e->getMessage();}
+
+}
--- mvc/view/usuario.php
+++ mvc/view/usuario.php
@@ -3,7 +3,12 @@
         Nombre:<input name="name" value="<?php echo $resultat_usuari[0]['NOM'];?>"><br><br>
         Mail:<input name="mail" value="<?php echo $resultat_usuari[0]['MAIL'];?>"><br><br>
         Dirección:<input name="address" value="<?php echo $resultat_usuari[0]['ADDRESS'];?>"><br><br>
+        Foto de perfil:<?php if ($info_usuari[0]['imatge']!=NULL){?>
+        <img class="img_cuenta" src="<?php echo $filesPublicPath.$usuario[0]['IMG']?> " alt="<?php echo $usuario[0]['NAME']?>"/>
+        <?php }
+        ?>
+        <input type="file" name="img_perfil" />
         Contraseña:<input name="password" type="password"><br><br>
-        <input type="submit" value="Check-in"><br><br>
+        <input type="submit" value="Modificar datos"><br><br>
     </form>
 </section>
