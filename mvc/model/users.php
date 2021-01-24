847581764a0c38f96fa93ecb7a9f74d684f0fc75
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 16:32:50 +0900

    Some changes made

--- mvc/model/users.php
+++ mvc/model/users.php
@@ -3,11 +3,12 @@ function login(string $mail,string $password)
 {
     try {
         $connection=connect();
-        $query = $connection->prepare("SELECT * FROM CUSTOMER WHERE MAIL=" + $mail + "LIMIT 1");
+        $query = $connection->prepare("SELECT * FROM CUSTOMER WHERE MAIL=:MAIL LIMIT 1");
+        $query->bindValue(':MAIL', $mail);
         $query->execute();
         print_r($query);
         $result = $query->fetch(PDO::FETCH_ASSOC);
-        print_r("\n");
+        print("\n");
         print_r($result);
         if ($result == false) return null;
         return password_verify($password,$result['PASSWORD']) ? $result : 0;
