bca7c3d4810fcfdefe7da103d7d0ad23849fe18c
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 17:42:50 +0900

    Some changes made

--- mvc/controller/login.php
+++ mvc/controller/login.php
@@ -10,7 +10,7 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     if ($user) {
         session_start();
         $_SESSION['ID'] = $user['ID'];
-        require __DIR__.'/categories.php';
+        GOTO("index.php");
     } else {
         echo "Error login";
         require __DIR__.'/../view/login.php';
