f3d677aceff24936ad18b19aea6c1340abc31557
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 20:07:45 +0900

    Some changes made

--- mvc/model/product.php
+++ mvc/model/product.php
@@ -2,13 +2,12 @@
 require_once __DIR__ . '/connection.php';
 function getProduct($id,$connexio){
     try {
-        $consulta_product = $connexio->prepare("SELECT * from PRODUCTS where ID=".$id);
-        $consulta_product->execute();
-        $product_resultat = $consulta_product->fetchAll(PDO::FETCH_ASSOC);
+        $query = $connexio->prepare("SELECT * from PRODUCTS where ID=".$id);
+        $query->execute();
+        $product = $consulta_product->fetchAll(PDO::FETCH_ASSOC);
+        return $product[0];
     } catch
     (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
-
-    return ($product_resultat[0]);
 }
