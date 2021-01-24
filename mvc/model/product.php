79ccb94e87e6a59163c894521af0865da0657dc6
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 20:08:21 +0900

    Some changes made

--- mvc/model/product.php
+++ mvc/model/product.php
@@ -4,7 +4,7 @@ function getProduct($id,$connexio){
     try {
         $query = $connexio->prepare("SELECT * from PRODUCTS where ID=".$id);
         $query->execute();
-        $product = $consulta_product->fetchAll(PDO::FETCH_ASSOC);
+        $product = $query->fetchAll(PDO::FETCH_ASSOC);
         return $product[0];
     } catch
     (PDOException $e) {
