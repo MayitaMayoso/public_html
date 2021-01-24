51b86fd64dda5f979ac8ee26d86d6e214c601d37
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 17:25:20 +0900

    Some changes made

--- mvc/view/navigator.php
+++ mvc/view/navigator.php
@@ -24,12 +24,12 @@ $(document).ready(
 
 <ul class="nav-links">
         <?php if (isset($_SESSION['ID'])) { ?>
-                <?php echo $_SESSION['ID'] ?>
+                <?php echo $_SESSION['ID']; ?>
                 <li><a id="account-link" href="./../index.php?action=cuenta">Mi cuenta</a></li>
                 <li><a id="orders-link" href="./../index.php?action=pedidos">Mis pedidos</a></li>
                 <li><a id="logout-link" href="./../index.php?action=logout">Cerrar sesión</a></li>
         <?php } else { ?>
-                <?php echo $_SESSION['ID'] ?>
+                <?php echo $_SESSION['ID']; ?>
                 <li><a id="checkin-link" href="#" target="_self">Check-in</a></li>
                 <li><a id="login-link" href="#" target="_self">Log-in</a></li>
         <?php } ?>
