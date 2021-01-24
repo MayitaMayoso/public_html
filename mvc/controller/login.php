4e054de6ad37da8c001ea9d6e805f5cce1b77d07
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 16:01:33 +0900

    Some changes made

--- mvc/controller/login.php
+++ mvc/controller/login.php
@@ -4,16 +4,9 @@ require_once __DIR__.'/../model/users.php';
 
 
 if($_SERVER['REQUEST_METHOD']=='POST') {
-    var_dump($_POST);
-    $filters = filter_input_array(
-        INPUT_POST, [
-            'MAIL' => FILTER_DEFAULT,
-            'PASSWORD' => FILTER_DEFAULT,
-        ]
-    );
-
-    $mail = $filters['MAIL'];
-    $password = $filters['PASSWORD'];
+    print_r($_POST);
+    $mail = htmlentities($_POST['MAIL'], ENT_QUOTES | ENT_HTML5, 'UTF-8');    
+    $password = htmlentities($_POST['password'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
     $user = login($mail, $password);
     if ($user) {
         var_dump($user);
