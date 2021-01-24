1f9fdd5e5b7f13a0eba7b86be7663f55a2dedb8c
1528948 <alberto.vallespin@e-campus.uab.cat>
2021-01-24 20:36:48 +0100

    css

--- mvc/controller/account.php
+++ mvc/controller/account.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../model/connection.php';
 require_once __DIR__.'/../model/account.php';
 $connexio=connect();
 $id=$_SESSION['ID'];
-$account=account($connexio,$id);
+$resultat_usuari=account($connexio,$id);
 require_once __DIR__.'/../view/account.php';
 
 
