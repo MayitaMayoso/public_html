54eaa183c469e205d8cd206b4f46b3a1f878d8fd
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 16:44:17 +0900

    Some changes made

--- mvc/controller/checkin.php
+++ mvc/controller/checkin.php
@@ -5,12 +5,12 @@ require_once __DIR__.'/../model/checkin.php';
 if($_SERVER['REQUEST_METHOD']=='POST') {
     $name = htmlentities($_POST['name'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
     $mail = htmlentities($_POST['mail'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
-    print_r($mail);
     $password = htmlentities($_POST['password'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
     $address = htmlentities($_POST['address'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
     $hash = password_hash($password, PASSWORD_DEFAULT);
     $connexio = connect();
     if (registre($name, $mail, $hash, $address, $connexio)) {
+        print_r($mail);
     	require __DIR__.'/../view/checkindata.php';
 	} else {
         echo "Error de registro";
