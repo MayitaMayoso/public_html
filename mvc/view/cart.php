340c3c6a1bb339026069b509013e8fa3b14aa4ad
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 21:08:20 +0900

    Some changes made

--- mvc/view/cart.php
+++ mvc/view/cart.php
@@ -5,11 +5,11 @@
         }
     );
 </script>
-
+<h2> Shopping Cart </h2>
 <?php $TOTAL_PRICE=0; $TOTAL_PRODUCTS=0; ?>
 <?php foreach ($products as $prod):?>
     <?php $TOTAL_PRICE+=($prod['TOTAL']*$prod['PRICE']); $TOTAL_PRODUCTS+=$prod['TOTAL']; ?>
-    <div class="floating-cart-product"><h3>
+    <div class="cart-product"><h3>
         <?php echo $prod['NAME']." x".$prod['TOTAL']."  ".($prod['TOTAL']*$prod['PRICE'])."€"?>
     </h3></div>
 <?php endforeach; ?>
