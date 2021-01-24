0bfc94e5ee35b4de4428f75c218b44673ad6d210
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 19:54:03 +0900

    Some changes made

--- mvc/view/floating-cart.php
+++ mvc/view/floating-cart.php
@@ -2,7 +2,7 @@
 <?php echo "Total: ".count($products)." productos";?>
 <?php $TOTAL_PRICE=0;?>
 <?php foreach ($products as $prod):?>
-    <?php $TOTAL_PRICE+=$prod["PRICE"]; ?>
+    <?php $TOTAL_PRICE+=($prod['TOTAL']*$prod['PRICE']); ?>
     <div class="floating-cart-product"><h3>
         <?php echo $prod['NAME']." x".$prod['TOTAL']."  ".($prod['TOTAL']*$prod['PRICE'])."€"?>
     </h3></div>
