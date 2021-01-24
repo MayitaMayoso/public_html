44fe99d11cc64997d1ba3e1e0f6100cd2b5133d5
1528948 <alberto.vallespin@e-campus.uab.cat>
2021-01-24 23:53:47 +0100

    css

--- mvc/view/account.php
+++ mvc/view/account.php
@@ -1,6 +1,6 @@
 
 <section id="account">
-    <h2>Mi cuenta</h2>
+    <h2>Mi cuenta</h2><br>
     <form id="edit" method="post" action="index.php?action=modify" enctype="multipart/form-data">
         Nombre: <input name="NAME" value="<?php echo $resultat_usuari[0]['NAME'];?>"><br><br>
         Mail: <input name="MAIL" value="<?php echo $resultat_usuari[0]['MAIL'];?>"><br><br>
@@ -12,6 +12,6 @@
         ?><br><br>
         <input type="file" name="IMG" /><br><br>
         Contraseña: <input name="PASSWORD" type="password"><br><br>
-        <input id="button-submit"type="submit" value="Modificar datos"><br><br>
+        <input id="button-submit" type="submit" value="Modificar datos"><br><br>
     </form>
 </section>
