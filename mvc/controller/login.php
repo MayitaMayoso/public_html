2044a8c7197232cbb55498c07a4aaf8475a406c9
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 16:02:24 +0900

    Some changes made

--- mvc/controller/login.php
+++ mvc/controller/login.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/../model/users.php';
 if($_SERVER['REQUEST_METHOD']=='POST') {
     print_r($_POST);
     $mail = htmlentities($_POST['MAIL'], ENT_QUOTES | ENT_HTML5, 'UTF-8');    
-    $password = htmlentities($_POST['password'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+    $password = htmlentities($_POST['PASSWORD'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
     $user = login($mail, $password);
     if ($user) {
         var_dump($user);
