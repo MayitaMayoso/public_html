74d99f9a9f643d057dbf897b5b1a1302936f311c
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 20:51:34 +0900

    Some changes made

--- mvc/controller/floating-cart.php
+++ mvc/controller/floating-cart.php
@@ -1,6 +1,6 @@
 <?php
-
-    session_start();
+    if (!isset($_SESSION['ID']))
+        session_start();
 	require_once __DIR__ . '/../model/connection.php';
 	require_once __DIR__.'/../model/order.php';
     $connection = connect();    
