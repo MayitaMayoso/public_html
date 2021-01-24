5fa13f88e8119fe0f91e893eba2033514fd8cb68
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 17:17:00 +0900

    Some changes made

--- mvc/controller/login.php
+++ mvc/controller/login.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     if ($user) {
         echo "Login correcto";
         $_SESSION['ID'] = $user['ID'];
-        require __DIR__.'../../content.php?action=null';
+        require __DIR__.'/categories.php';
     } else {
         echo "Error login";
         require __DIR__.'/../view/login.php';
