9cd1e880b16f7324b35a91239b884fee9f26c8db
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 20:40:10 +0900

    Some changes made

--- mvc/model/addProduct.php
+++ mvc/model/addProduct.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../model/connection.php';
 require_once __DIR__.'/../model/order.php';
 
+session_start();
 $connection = connect();    
 $order = getOrder($connection);
 addProduct($_GET["product"], $order, $connection);
