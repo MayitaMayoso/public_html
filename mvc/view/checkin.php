0e781efc49b83d2bf3ff75df0dff73d6851da391
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 14:29:23 +0900

    Some changes made

--- mvc/view/checkin.php
+++ mvc/view/checkin.php
@@ -1,7 +1,13 @@
-<form method="post" action="index.php?action=checkin">
+<script type="text/javascript">
+    function checkinFunction() {
+        $("#content").load("content.php?action=checkin");
+    }
+</script>
+
+<form id="checkin-form" method="post">
     <input placeholder="Nombre" name="name" type="text"><br><br>
     <input placeholder="Mail" name="mail" type="email"><br><br>
     <input placeholder="Dirección" name="address" type="text"><br><br>
     <input placeholder="Contraseña" name="password" type="password"><br><br>
-    <input type="submit" value="Check-in"><br><br>
+    <input type="submit" value="Check-in" onclick="checkinFunction()"><br><br>
 </form>
