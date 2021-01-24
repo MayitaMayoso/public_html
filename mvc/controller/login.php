f9d3ca34f792a194ade905d2157485d8894aff18
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 15:56:24 +0900

    Some changes made

--- mvc/controller/login.php
+++ mvc/controller/login.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../model/users.php';
 
 
 if($_SERVER['REQUEST_METHOD']=='POST') {
-    if($_SERVER['REQUEST_METHOD']=='POST') {
+    echo $_GET;
     $filters = filter_input_array(
         INPUT_POST, [
             'MAIL' => FILTER_DEFAULT,
@@ -16,18 +16,14 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     $password = $filters['PASSWORD'];
     $user = login($mail, $password);
     if ($user) {
-    	var_dump($user);
-    	$_SESSION['ID'] = $user['ID'];
-    	echo "te has logueado";
+        var_dump($user);
+        $_SESSION['ID'] = $user['ID'];
+        echo "te has logueado";
+    } else {
+        var_dump($user);
+        echo "error de login";
+        __DIR__.'/../view/login.php';
     }
-    else {
-    	var_dump($user);
-    	echo "error de login";
-    	__DIR__.'/../view/login.php';
-    }
-    #return;
-}
-}
-else {
+} else {
 	require __DIR__.'/../view/login.php';
 }
