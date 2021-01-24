0637562ab994efcc962740363af60c07e39e02f0
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 23:20:18 +0900

    Some changes made

--- mvc/controller/checkin.php
+++ mvc/controller/checkin.php
@@ -3,16 +3,21 @@ require_once __DIR__.'/../model/connection.php';
 require_once __DIR__.'/../model/checkin.php';
 
 if($_SERVER['REQUEST_METHOD']=='POST') {
-    $name = htmlentities($_POST['name'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
-    $mail = htmlentities($_POST['mail'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
-    $password = htmlentities($_POST['password'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
-    $address = htmlentities($_POST['address'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-    $connexio = connect();
-    if (registre($name, $mail, $hash, $address, $connexio)) {
-    	require __DIR__.'/../view/checkindata.php';
+    $password = filter_var($_POST['password'], FILTER_VALIDATE_REGEX,
+    array("options"=>array("regexp"=>"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"))));
+
+    if ($password) {
+        $name = htmlentities($_POST['name'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        $mail = htmlentities($_POST['mail'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        $password = htmlentities($_POST['password'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        $address = htmlentities($_POST['address'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $connexio = connect();
+        registre($name, $mail, $hash, $address, $connexio)
+        require __DIR__.'/../view/checkindata.php';
 	} else {
-        echo "Error de registro";
+        echo "Error de registro contraseña muy floja, utilice al menos una mayúscula, minúscula, número, símbolo y 8 carácteres.";
+        require __DIR__.'/../view/checkin.php';
     }
 } else {
     require __DIR__.'/../view/checkin.php';
