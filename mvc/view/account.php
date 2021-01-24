504c16867fa2f89352ec62b62f5baf4f686c2574
1528948 <alberto.vallespin@e-campus.uab.cat>
2021-01-24 20:29:21 +0100

    css

--- mvc/view/account.php
+++ mvc/view/account.php
@@ -1,7 +1,17 @@
-<form id="account-form" method="POST" action="index.php?action=account">
-    <input placeholder="Nombre" name="name" type="text"><br><br>
-    <input placeholder="Mail" name="mail" type="email"><br><br>
-    <input placeholder="Dirección" name="address" type="text"><br><br>
-    <input placeholder="Contraseña" name="password" type="password"><br><br>
-    <input type="submit" value="Actualizar información"><br><br>
-</form>
+
+<section id="account">
+    <h2>Mi cuenta</h2>
+    <form id="edit" method="post" action="index.php?action=modify" enctype="multipart/form-data">
+        Nombre:<input name="name" value="<?php echo $resultat_usuari[0]['NOM'];?>"><br><br>
+        Mail:<input name="mail" value="<?php echo $resultat_usuari[0]['MAIL'];?>"><br><br>
+        Dirección:<input name="address" value="<?php echo $resultat_usuari[0]['ADDRESS'];?>"><br><br>
+        Foto de perfil:
+        <?php if ($resultat_usuari[0]['IMG']!=NULL){?>
+        <img class="foto_perfil" src="<?php echo $fotousuario.$resultat_usuari[0]['IMG']?> " alt="<?php echo $resultat_usuari[0]['NAME']?>"/>
+        <?php }
+        ?>
+        <input type="file" name="img_user" />
+        Contraseña:<input name="password" type="password"><br><br>
+        <input type="submit" value="Modificar datos"><br><br>
+    </form>
+</section>
