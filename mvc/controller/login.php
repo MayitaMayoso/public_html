53bda62553c2f9ff84d3b3ca3f0d2b543f5c1060
Agustín Molina Uasuf <molina.uasuf@gmail.com>
2021-01-24 17:09:23 +0900

    Some changes made

--- mvc/controller/login.php
+++ mvc/controller/login.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../model/users.php';
 
 
 if($_SERVER['REQUEST_METHOD']=='POST') {
+    echo "pasamos por aqui";
     $mail = htmlentities($_POST['MAIL'], ENT_QUOTES | ENT_HTML5, 'UTF-8');    
     $password = htmlentities($_POST['PASSWORD'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
     $user = login($mail, $password);
